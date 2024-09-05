ad91d5975b935a8364f7704c866dbabd422c6fb9
Jero4204 <jeronimo.sanchez99@eia.edu.co>
2024-09-05 09:31:15 -0500

    agregamos el metodo para buscar las parejas

--- src/components/ShowQuote.tsx
+++ src/components/ShowQuote.tsx
@@ -1,9 +1,14 @@
 import { useState,useEffect } from 'react';
 import { PlayerNBA } from '../models/player.model';
+import { Respuesta } from '../models/respuesta.model';
 
 export const ShowQuote = () => {
     const [values, setValues] = useState<PlayerNBA[]>([]);
     const [valuesOrg, setValuesOrg] = useState<PlayerNBA[]>([]);
+    const [respuesta, setRespuesta] = useState<Respuesta[]>([]);
+
+    let menorestatura = 139;
+    let mayorestatura = 90;
    
     const getValues = async () => {
         const apiUrl = 'https://mach-eight.uc.r.appspot.com/';
@@ -28,6 +33,27 @@ export const ShowQuote = () => {
 
     const getValuesOrg = async () => {
         setValuesOrg(values.sort((a, b) => Number(a.h_in) - Number(b.h_in)));
+    }
+
+    const encontrarParejas = async (estaturaPareja: number) => {
+        if(estaturaPareja >= menorestatura && estaturaPareja <= mayorestatura){
+            let i = 0;
+            let respuestas: Respuesta[] = [];
+            while(i<valuesOrg.length){
+                let estaturaRequerida = estaturaPareja-Number(valuesOrg[i].h_in);
+                let w = i+1;
+                while(estaturaRequerida >= Number(valuesOrg[w].h_in)){
+                    if(estaturaRequerida = Number(valuesOrg[w].h_in)){
+                        respuestas.push({first_name1: valuesOrg[i].first_name, last_name1: valuesOrg[i].last_name, first_name2: valuesOrg[w].first_name, last_name2: valuesOrg[w].last_name})
+                    }
+                    w = w+1;
+                }
+                i = i+1;
+            }
+            setRespuesta(respuestas);
+        }else{
+            console.log("No hay parejas con esa estatura")
+        }
     }
 
     const boton = async () => {
---
+++ src/models/respuesta.model.ts
@@ -0,0 +1,7 @@
+
+export interface Respuesta{
+    first_name1: string;
+    last_name1: string;
+    first_name2: string;
+    last_name2: string;
+}
